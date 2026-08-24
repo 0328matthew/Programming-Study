337c976e5070f63b48daec42beddb260fa9d816f
0328matthew <jgh109702@pusan.ac.kr>
2026-08-24 19:48:37 +0900

    vault sync 2026-08-24 19:48

--- C++/예제/Chapter 8/Problem1.cpp
+++ C++/예제/Chapter 8/Problem1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     int array[100] = {0};
-    srand((unsigned int)time(NULL));
+    std::srand
     for(int i = 0; i < 100; i++) {
         array[i] = rand();
     }
