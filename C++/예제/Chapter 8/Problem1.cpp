7ff91577d924ae2a1023c25646de631f7068f7cb
0328matthew <jgh109702@pusan.ac.kr>
2026-08-24 19:54:15 +0900

    vault sync 2026-08-24 19:54

--- C++/예제/Chapter 8/Problem1.cpp
+++ C++/예제/Chapter 8/Problem1.cpp
@@ -7,13 +7,13 @@ void print();
 
 int main() {
     int array[100] = {0};
-    srand(static_cast<unsigned>(time(nullptr)))
+    srand(static_cast<unsigned>(time(nullptr)));
     for(int i = 0; i < 100; i++) {
         array[i] = 100 + rand() % 101;
     }
 }
 
-void print() {
+void print(const int ) {
     int num = 0;
     cout << "이 배열의 " << num << "번째 요소는 " << array[num] << " 입니다.";
 }
