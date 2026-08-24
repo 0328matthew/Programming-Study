4cb1fbc0a2e88296568122fcda014c29e6c7c2e7
0328matthew <jgh109702@pusan.ac.kr>
2026-08-24 19:49:15 +0900

    vault sync 2026-08-24 19:49

--- C++/예제/Chapter 8/Problem1.cpp
+++ C++/예제/Chapter 8/Problem1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     int array[100] = {0};
-    std::srand
+    srand(static_cast<unsigned>)
     for(int i = 0; i < 100; i++) {
         array[i] = rand();
     }
