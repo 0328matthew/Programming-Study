dbeb9eca4fab3a052508ea915b7dc0fad697c8d4
0328matthew <jgh109702@pusan.ac.kr>
2026-08-25 18:11:36 +0900

    vault sync 2026-08-25 18:11

--- C++/예제/Chapter 8/Problem7/Problem7.cpp
+++ C++/예제/Chapter 8/Problem7/Problem7.cpp
@@ -18,7 +18,7 @@ int main() {
     }
     for(int i = 0; i < 20; i++) {
         if(array[i] % 2 == 0) {
-            evenArray[J] = evenArray[i];
+            evenArray[j] = evenArray[i];
             j++;
         }
         else {
@@ -35,7 +35,7 @@ int main() {
         cout << "짝수 배열의 " << i << "번째 요소는 " << evenArray[i] << " 입니다." << endl;
     }
     cout << endl;
-    for(int i = 0; i < k; k++) {
+    for(int i = 0; i < k; i++) {
         cout << "홀수 배열의 " << i << "번째 요소는 " << oddArray[i] << " 입니다." << endl;
     }
 
