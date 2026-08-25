e2607359d031cdee9cd16eeff94742a1de90fdd7
0328matthew <jgh109702@pusan.ac.kr>
2026-08-25 18:05:00 +0900

    vault sync 2026-08-25 18:05

--- C++/예제/Chapter 8/Problem7/Problem7.cpp
+++ C++/예제/Chapter 8/Problem7/Problem7.cpp
@@ -7,12 +7,15 @@ using namespace std;
 int main() {
     int array[20] = {0};
     int oddArray[20] = {0};
-    int evenArray[20]
+    int evenArray[20] = {0};
 
     srand(static_cast<unsigned>(time(nullptr)));
 
     for(int i = 0; i < 20; i++) {
         array[i] = rand() %101;
+    }
+    for(int i = 0; i < 20; i++) {
+        if(array[i])
     }
 
 }
