f682c906d6835345d148d0b14b4914db1c43f1c2
0328matthew <jgh109702@pusan.ac.kr>
2026-08-26 16:31:55 +0900

    vault sync 2026-08-26 16:31

--- C++/예제/Chapter 8/Problem9/Problem9.cpp
+++ C++/예제/Chapter 8/Problem9/Problem9.cpp
@@ -13,7 +13,8 @@ int main() {
     srand(static_cast<unsigned>(time(nullptr)));
 
     for(int i = 0; i < SIZE; i++) {
-        array1[SIZE] = rand();
+        array1[SIZE] = 100 + rand() % 100;
+        array2[SIZE] = 100 + rnad() % 100;
     }
 
 }
