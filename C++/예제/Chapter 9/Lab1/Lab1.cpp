ff57d12059eaa1d75f5309a1038951465f34f38f
0328matthew <jgh109702@pusan.ac.kr>
2026-08-28 21:48:47 +0900

    vault sync 2026-08-28 21:48

--- C++/예제/Chapter 9/Lab1/Lab1.cpp
+++ C++/예제/Chapter 9/Lab1/Lab1.cpp
@@ -5,7 +5,9 @@
 Course::Course(int num, const char* ifn) : numOfStds(num), inputFileName(ifn); {
     inputFile.open(inputFileName);
     students = new Student[numOfStds];
-    
+    getInput();
+    setGrades();
+    setAverage();
 
 
 
