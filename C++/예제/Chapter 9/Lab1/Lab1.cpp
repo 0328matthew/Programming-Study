6afdf75834dca051890a2b05b3e6c3cf2785065b
0328matthew <jgh109702@pusan.ac.kr>
2026-08-28 22:03:15 +0900

    vault sync 2026-08-28 22:03

--- C++/예제/Chapter 9/Lab1/Lab1.cpp
+++ C++/예제/Chapter 9/Lab1/Lab1.cpp
@@ -51,7 +51,7 @@ void Course::setDeviations() {
 
 void Course::printResult() const [
     cout << endl;
-    cout << "ID    점수    등급    편차" << endl;
+    cout << " ID   점수   등급    편차" << endl;
     cout << "---- ----- ----- -------" << endl;
-    
+
 ]
