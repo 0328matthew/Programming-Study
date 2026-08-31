797b0ffdd276c142f337a8944a617cba5672d987
0328matthew <jgh109702@pusan.ac.kr>
2026-08-31 20:26:27 +0900

    vault sync 2026-08-31 20:26

--- C++/예제/Chapter 9/Problem1/Problem1.cpp
+++ C++/예제/Chapter 9/Problem1/Problem1.cpp
@@ -8,7 +8,13 @@ int getMax(int a, int b, int c);
 int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    int a = rand()
+    int a = rand();
+    int b = rand();
+    int c = rand();
+
+    int getMax(a, b, c);
+
+    cout << 
 }
 
 int getMax(int a, int b, int c) {
