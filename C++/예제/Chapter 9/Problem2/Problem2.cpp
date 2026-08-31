02456205eb0601a630e6e93bd579371f16f7d8b7
0328matthew <jgh109702@pusan.ac.kr>
2026-08-31 20:40:11 +0900

    vault sync 2026-08-31 20:40

--- C++/예제/Chapter 9/Problem2/Problem2.cpp
+++ C++/예제/Chapter 9/Problem2/Problem2.cpp
@@ -7,5 +7,11 @@ using namespace std;
 double& getMax(double& a, double& b, double& c);
 
 int main() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    int num1 = rand();
+    int num2 = rand();
+    int num3 = rand();
+
     
 }
