6e22fb20f58d24d473880be644f12dec48129ced
0328matthew <jgh109702@pusan.ac.kr>
2026-08-31 20:41:07 +0900

    vault sync 2026-08-31 20:41

--- C++/예제/Chapter 9/Problem2/Problem2.cpp
+++ C++/예제/Chapter 9/Problem2/Problem2.cpp
@@ -9,11 +9,11 @@ double& getMax(double& a, double& b, double& c);
 int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    int num1 = rand();
-    int num2 = rand();
-    int num3 = rand();
+    double num1 = rand();
+    double num2 = rand();
+    double num3 = rand();
 
     cout << num1 << "  " << num2 << " " << num3 << " " << endl;
 
-    
+    double maxNum = getMax&
 }
