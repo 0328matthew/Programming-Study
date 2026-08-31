0ed5236510aeba92b6b78264bb06b1eb070b3be5
0328matthew <jgh109702@pusan.ac.kr>
2026-08-31 21:41:32 +0900

    vault sync 2026-08-31 21:41

--- C++/예제/Chapter 9/Problem4/Problem4.cpp
+++ C++/예제/Chapter 9/Problem4/Problem4.cpp
@@ -5,5 +5,11 @@ int main() {
     Array array1(4);
 
     array1.insert(10);
-    array1.insert(20)
+    array1.insert(20);
+    array1.insert(30);
+
+    array1.print();
+
+    system("pause");
+    return 0;
 }
