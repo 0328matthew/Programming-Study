77260c336055c78ec8b959e46bc4fa6723a7476c
0328matthew <jgh109702@pusan.ac.kr>
2026-08-31 20:35:40 +0900

    vault sync 2026-08-31 20:35

--- C++/예제/Chapter 9/Problem1/Problem1.cpp
+++ C++/예제/Chapter 9/Problem1/Problem1.cpp
@@ -4,7 +4,7 @@
 #include <ctime>
 using namespace std;
 
-int getMax(int& a, int& b, int& c);
+int& getMax(int& a, int& b, int& c);
 
 int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
