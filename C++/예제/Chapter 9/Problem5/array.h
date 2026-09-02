4e265caa6e58a0ff4e60cc697ccc9d1b46570568
0328matthew <jgh109702@pusan.ac.kr>
2026-09-02 19:03:25 +0900

    vault sync 2026-09-02 19:03

--- C++/예제/Chapter 9/Problem5/array.h
+++ C++/예제/Chapter 9/Problem5/array.h
@@ -2,9 +2,8 @@
 using namespace std;
 
 #ifndef SORTEDARRAY_H
-#define SORTEDARRAY_H
+#define SORETEARRAY_H
 
-// 불변식(invariant): 항상 arr[0] <= arr[1] <= ... <= arr[size-1]
 class SortedArray {
 
     private:
@@ -12,14 +11,15 @@ class SortedArray {
         int capacity;
         int size;
         int* arr;
+        int idx;
 
     public:
-
+        
         SortedArray(int capacity);
         ~SortedArray();
-
-        void insert(int value);   // 정렬 상태를 유지하며 알맞은 위치에 삽입
-        void remove(int idx);     // idx번째 원소를 지우고 뒤를 당겨 구멍을 메움
+        
+        void insert(int value);
+        void remove(int idx);
         void print() const;
 
 };
